1f5317bc51e398e180b166bb783cda5f30b626a5
pgajek <pgajek94@gmail.com>
2024-07-03 11:28:59 +0200

    first funcitons

--- src/Counter.sol
+++ src/Counter.sol
@@ -2,13 +2,87 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
-    uint256 public number;
-
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    struct VariableData {
+        uint256 slotIndex;
+        uint256 offset;
+        uint256 length;
     }
 
-    function increment() public {
-        number++;
+    struct VariableRecord {
+        string variableName;
+        VariableData variableData;
     }
+
+    mapping(address => mapping(string => VariableData)) public contracts;
+
+    function addRecords(
+        address contractAddress,
+        VariableRecord[] memory records
+    ) public {
+        for (uint256 i = 0; i < records.length; i++) {
+            contracts[contractAddress][records[i].variableName] = records[i]
+                .variableData;
+        }
+    }
+
+    function computePrimitiveSlot(
+        address contractAddress,
+        string calldata key
+    ) public view returns (VariableData memory) {
+        return contracts[contractAddress][key];
+    }
+
+    // function computeFixedArraySlot(
+    //     address contractAddress,
+    //     string calldata key
+    // ) public view returns (uint) {
+    //     VariableData memory data = contracts[contractAddress][key];
+    //      uint256 slotIndex = data.slotIndex;
+    //     uint256 offset = data.offset;
+    //     uint256 length = data.length;
+
+    //     // return baseSlot + index;
+    // }
+
+    // function computeDynamicArrayElementSlot(
+    //     address contractAddress,
+    //     string calldata key
+    // ) public view returns (uint) {
+    //     VariableData memory data = contracts[contractAddress][key];
+    //      uint256 slotIndex = data.slotIndex;
+    //     uint256 offset = data.offset;
+    //     uint256 length = data.length;
+
+    //     return uint(keccak256(abi.encodePacked(baseSlot))) + index;
+    // }
+
+    function computeMappingSlot(
+        address contractAddress,
+        string calldata key
+    ) public view returns (VariableData memory) {
+        VariableData memory data = contracts[contractAddress][key];
+
+        uint256 slotIndex = data.slotIndex;
+
+        data.slotIndex = uint(keccak256(abi.encodePacked(key, slotIndex)));
+
+        return data;
+    }
+
+    // function computePackedArraySlot(
+    //     uint baseSlot,
+    //     uint index,
+    //     uint elementSize
+    // ) public pure returns (uint) {
+    //     uint elementsPerSlot = 32 / elementSize;
+    //     return baseSlot + (index / elementsPerSlot);
+    // }
+
+    // function computePackedArrayOffset(
+    //     uint index,
+    //     uint elementSize
+    // ) public pure returns (uint) {
+    //     uint elementsPerSlot = 32 / elementSize;
+    //     return (index % elementsPerSlot) * elementSize;
+    // }
 }
